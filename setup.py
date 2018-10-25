666bc63925b8dab32e9ea9925b32de7b5071fcb1
Ashley Gould <agould@ucop.edu>
2018-10-24 20:27:49 -0700

    reorder functions in tag_crawler

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="tag_crawler",
+    version='0.0',
+    py_modules=['tag_crawler'],
+    install_requires=[
+        'Click',
+        'boto3',
+        'organizer',
+    ],
+    entry_points='''
+        [console_scripts]
+        tag_crawler=tag_crawler:cli
+    ''',
+)
--- tag_crawler.py
+++ tag_crawler.py
@@ -23,15 +23,44 @@ import boto3
 from organizer import crawlers, orgs, utils
 
 
-def munge_tag_filter(tag_filter):
-    _filter = []
-    for tag in tag_filter:
-        key, sep, value = tag.partition(',')
-        if value:
-            _filter.append(dict(Key=key, Values=[value]))
-        else:
-            _filter.append(dict(Key=key))
-    return _filter
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option('--role', '-r',
+    required=True,
+    help='IAM role for accessing AWS Organization accounts',
+)
+@click.option('--resource-filter', '-rf', 'resource_filter',
+    multiple=True,
+    help='AWS resource name to filter by. Can be used multiple times.',
+)
+@click.option('--tag-filter', '-tf', 'tag_filter',
+    multiple=True,
+    help='Tag to filter by. Can be used multiple times. Must be a string. '
+    'Can be either a key name or a key/value pair saparated by a comma. '
+    'Tags containing spaces must be quoted.',
+)
+@click.option('--show-resource-only',
+    is_flag=True,
+    help='Display only the ARN of matching resources.',
+)
+@click.option('--show-keys-only',
+    is_flag=True,
+    help='Display only tag key names.',
+)
+def cli(role, tag_filter, resource_filter, show_resource_only, show_keys_only):
+    #click.echo(tag_filter)
+    #click.echo(resource_filter)
+    filters = parse_filters(tag_filter, resource_filter)
+    #click.echo(utils.yamlfmt(filters))
+    crawler = get_crawler(role)
+    if show_keys_only:
+        execution = crawler.execute(get_tag_keys)
+    else:
+        execution = crawler.execute(get_tagged_resources, filters, show_resource_only)
+    output = output_regions_per_account(execution)
+    click.echo(utils.yamlfmt(output))
 
 
 def parse_filters(tag_filter, resource_filter):
@@ -43,11 +72,48 @@ def parse_filters(tag_filter, resource_filter):
     return filters
 
 
-def munge_tag_map(response, show_resource_only):
-    if show_resource_only:
-        return [tag_map['ResourceARN'] for tag_map in response['ResourceTagMappingList']]
-    else:
-        return response['ResourceTagMappingList']
+def munge_tag_filter(tag_filter):
+    _filter = []
+    for tag in tag_filter:
+        key, sep, value = tag.partition(',')
+        if value:
+            _filter.append(dict(Key=key, Values=[value]))
+        else:
+            _filter.append(dict(Key=key))
+    return _filter
+
+
+def get_crawler(org_access_role):
+    master_account_id = utils.get_master_account_id(org_access_role)
+    my_org = orgs.Org(master_account_id, org_access_role)
+    my_org.load()
+    my_crawler = crawlers.Crawler(
+        my_org,
+        access_role=org_access_role,
+        accounts=['ait-poc', 'ashley-training', 'ucop-its'],
+        regions=['us-west-2', 'us-east-1'],
+    )
+    my_crawler.load_account_credentials()
+    return my_crawler
+
+
+def get_tag_keys(region, account):
+    """
+    Crawler payload function
+    """
+    client = boto3.client(
+        'resourcegroupstaggingapi',
+        region_name=region,
+        **account.credentials
+    )
+    response = client.get_tag_keys()
+    tag_key_list = response['TagKeys']
+    while response['PaginationToken']:
+        response = client.get_tag_keys(
+            PaginationToken=response['PaginationToken'],
+        )
+        tag_key_list += response['TagKeys']
+    return dict(TagKeys=tag_key_list)
 
 
 def get_tagged_resources(region, account, filters, show_resource_only):
@@ -55,8 +121,8 @@ def get_tagged_resources(region, account, filters, show_resource_only):
     Crawler payload function
     """
     client = boto3.client(
-        'resourcegroupstaggingapi', 
-        region_name=region, 
+        'resourcegroupstaggingapi',
+        region_name=region,
         **account.credentials,
     )
     response = client.get_resources(
@@ -73,51 +139,11 @@ def get_tagged_resources(region, account, filters, show_resource_only):
     return {label: tag_mapping_list}
 
 
-def get_tag_keys(region, account):
-    """
-    Crawler payload function
-    """
-    client = boto3.client(
-        'resourcegroupstaggingapi', 
-        region_name=region, 
-        **account.credentials,
-    )
-    response = client.get_tag_keys()
-    tag_key_list = response['TagKeys']
-
-    while response['PaginationToken']:
-        response = client.get_tag_keys(
-            PaginationToken=response['PaginationToken'],
-        )
-        tag_key_list += response['TagKeys']
-    return dict(TagKeys=tag_key_list)
-
-
-def get_crawler(org_access_role):
-    master_account_id = utils.get_master_account_id(org_access_role)
-    my_org = orgs.Org(master_account_id, org_access_role)
-    my_org.load()
-    my_crawler = crawlers.Crawler(
-        my_org,
-        access_role=org_access_role,
-        #accounts=['ait-poc', 'ashley-training', 'ucop-its'],
-        #regions=['us-west-2', 'us-east-1'],
-    )
-    my_crawler.load_account_credentials()
-    return my_crawler
-
-
-def purge_empty_responses(execution):
-    '''
-    Return list of execution responses for which output is not empty.
-    Expects each response to be a list of dict.
-    '''
-    responses = [
-        r for r in execution.responses 
-        if len(r.payload_output) == 1
-        and list() not in r.payload_output.values()
-    ]
-    return responses
+def munge_tag_map(response, show_resource_only):
+    if show_resource_only:
+        return [tag_map['ResourceARN'] for tag_map in response['ResourceTagMappingList']]
+    else:
+        return response['ResourceTagMappingList']
 
 
 def output_regions_per_account(execution):
@@ -137,44 +163,18 @@ def output_regions_per_account(execution):
     return(collector)
 
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
-
-@click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('--role', '-r', 
-    required=True,
-    help='IAM role for accessing AWS Organization accounts',
-)
-@click.option('--resource-filter', '-rf', 'resource_filter',
-    multiple=True,
-    help='AWS resource name to filter by. Can be used multiple times.',
-)
-@click.option('--tag-filter', '-tf', 'tag_filter',
-    multiple=True,
-    help='Tag to filter by. Can be used multiple times. Must be a string. '
-    'Can be either a key name or a key/value pair saparated by a comma. '
-    'Tags containing spaces must be quoted.',
-)
-@click.option('--show-resource-only', 
-    is_flag=True,
-    help='Display only the ARN of matching resources.',
-)
-@click.option('--show-keys-only',
-    is_flag=True,
-    help='Display only tag key names.',
-)
-def main(role, tag_filter, resource_filter, show_resource_only, show_keys_only):
-    #click.echo(tag_filter)
-    #click.echo(resource_filter)
-    filters = parse_filters(tag_filter, resource_filter)
-    #click.echo(utils.yamlfmt(filters))
-    crawler = get_crawler(role)
-    if show_keys_only:
-        execution = crawler.execute(get_tag_keys)
-    else:
-        execution = crawler.execute(get_tagged_resources, filters, show_resource_only)
-    output = output_regions_per_account(execution)
-    click.echo(utils.yamlfmt(output))
+def purge_empty_responses(execution):
+    '''
+    Return list of execution responses for which output is not empty.
+    Expects each response to be a list of dict.
+    '''
+    responses = [
+        r for r in execution.responses 
+        if len(r.payload_output) == 1
+        and list() not in r.payload_output.values()
+    ]
+    return responses
 
 
 if __name__ == "__main__":
-    main()
+    cli()
